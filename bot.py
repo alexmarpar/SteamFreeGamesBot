5b0047aba94d9c4b0fb64a3a2b70ce0515006f6d
alexmarpar <188632620+alexmarpar@users.noreply.github.com>
2026-06-13 18:22:15 +0200

    Added full free games scrapper

--- bot.py
+++ bot.py
@@ -10,10 +10,9 @@ DISCORDWEBHOOK = os.environ.get('DISCORD_WEBHOOK')
 request = requests.get(url)
 
 
-def sendjson(gametittle,linkgame,gameimage):
+def sendjson(gametittle,linkgame):
     data = {
         "content": f"Nuevo juego reclamable gratis en steam por tiempo limitado, es {gametittle} / link: {linkgame}",
-        "image": f"{gameimage}" #Image but with the link is enought
     }
     requests.post(DISCORDWEBHOOK, json=data)
 
@@ -22,24 +21,16 @@ if request.status_code == 200:
     html = request.text
     soup = bt(html, 'lxml')
     searcher = soup.find('div',id='search_result_container')
-    searcher_games = searcher.find_all('div', id='search_resultsRows')
+    games = searcher.find_all('a',class_='search_result_row')
 
-    for game in searcher_games:
-        # main div (game)
-        maintittles = game.find('a', class_='search_result_row ds_collapse_flag')
+    for game in games:
         # game tittle:
-        div_game_tittle = maintittles.find('div', 'search_name ellipsis')
+        div_game_tittle = game.find('div', 'search_name ellipsis')
         game_tittle_span = div_game_tittle.find('span')
         game_tittle = game_tittle_span.get_text(strip=True)
         # link to buy
-        link_buy = maintittles['href']
-        # game_image
-        search_capsules = maintittles.find('div', class_='search_capsule')
-        game_image_img = search_capsules.find('img')
-        game_image = game_image_img['src']
-        sendjson(game_tittle,link_buy,game_image)
+        link_buy = game.get('href')
+        sendjson(game_tittle,link_buy)
 
 else:
     print("Error, not status_code 200 detected")
-
-
